3c26301eb89cdcc7a414d5c3ed59fc285778052e
jiangtaidi <jiangtd@jiangtddemacbook-air.local>
2016-01-11 09:23:49 +0800

    Initial Commit

---
+++ CropDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CropDemo
+//
+//  Created by jiangtd on 16/1/11.
+//  Copyright © 2016年 jiangtd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
